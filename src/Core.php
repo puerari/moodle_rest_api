68faa1c25f671bf79fa5569767e48ce0df58eb8b
Leandro Puerari <puerari@gmail.com>
2021-12-14 19:20:50 -0300

    Implemented getCourses method.

--- src/Core.php
+++ src/Core.php
@@ -15,7 +15,7 @@ trait Core
      * @return array|string
      * @throws MraException
      */
-    public function getCoursesByField(/*string*/ $field, /*string*/ $value)
+    public function getCoursesByField(/*string*/ $field, /*string*/ $value)/*: array|string*/
     {
         $validFields = ['', 'id', 'ids', 'shortname', 'idnumber', 'category'];
         if (!in_array($field, $validFields)) {
@@ -23,6 +23,23 @@ trait Core
         }
         $this->data = compact('field', 'value');
         $this->data['wsfunction'] = 'core_course_get_courses_by_field';
+        return $this->execGetCurl();
+    }
+
+    /**
+     * @param array $ids
+     * @return array|string
+     * @throws MraException
+     */
+    public function getCourses(array $ids)
+    {
+        $this->data = [];
+        $this->data['wsfunction'] = 'core_course_get_courses';
+        $cont = 0;
+        foreach ($ids as $vlr) {
+            $this->data['options']['ids'][$cont] = intval($vlr);
+            ++$cont;
+        }
         return $this->execGetCurl();
     }
 }
