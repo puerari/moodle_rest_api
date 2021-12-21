a9829aa1510dfcde88bff9fc1696bea801030e0d
Leandro Puerari <puerari@gmail.com>
2021-12-20 23:13:21 -0300

    Implemented getEnrolledUsers and getEnrolledStudents method.

--- src/Core.php
+++ src/Core.php
@@ -42,4 +42,44 @@ trait Core
         }
         return $this->execGetCurl();
     }
+
+    /**
+     * @param $course_id
+     * @param array $options
+     * @return array|string
+     * @throws MraException
+     */
+    public function getEnrolledUsers(/*string*/ $course_id, array $options = [])
+    {
+        $this->data = [];
+        $this->data['courseid'] = $course_id;
+        $this->data['options'] = $options;
+        $this->data['wsfunction'] = 'core_enrol_get_enrolled_users';
+        return $this->execGetCurl();
+    }
+
+    /**
+     * @param $course_id
+     * @param array $options
+     * @return array
+     * @throws MraException
+     */
+    public function getEnrolledStudents(/*string*/ $course_id, array $options = [])/*: array*/
+    {
+        $oldReturnFormat = $this->getReturnFormat();
+        $this->setReturnFormat(self::RETURN_ARRAY);
+        $students = $this->getEnrolledUsers($course_id, $options);
+        $this->setReturnFormat($oldReturnFormat);
+        $studentsToReturn = [];
+        foreach ($students as $student) {
+            if (is_array($student['roles'])) {
+                foreach ($student['roles'] as $role) {
+                    if ($role['shortname'] == 'student') {
+                        $studentsToReturn[] = $student;
+                    }
+                }
+            }
+        }
+        return $studentsToReturn;
+    }
 }
